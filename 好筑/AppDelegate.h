c9d668fb695987e8a1cd132a8be9779a756ea910
zhangyun945 <472641929@qq.com>
2015-12-04 16:49:38 +0800

    Initial Commit

---
+++ 好筑/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  好筑
+//
+//  Created by tusm on 15/12/4.
+//  Copyright (c) 2015年 Zs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
